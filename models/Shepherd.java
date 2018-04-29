b8dce8c4c008481c49df0f159df06270a7f3a53e
Bjarke Sporring <work.bjarke@gmail.com>
2018-04-29 19:36:18 +0200

    Got a working base image loader. Working on player and camera implementation

---
+++ models/Shepherd.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.image.*;
+import javax.swing.*;
+
+public class Shepherd
+{
+    private int x, y;
+    private final int width = 200, height = 200;
+    private BufferedImage image;
+    private Camera camera;
+    private Board board;
+
+    public Shepherd(Board board)
+    {
+        this.x = board.getW() / 2;
+        this.y = board.getY() / 2;
+        camera = new Camera(x, y, board.getW(), board.getH());
+    }
+
+    public Shepherd(int x, int y, Board board)
+    {
+        this.x = x;
+        this.y = y;
+        this.board = board;
+        camera = new Camera(x, y, board.getW(), board.getH());
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public Image getImage() { return image; }
+
+}
